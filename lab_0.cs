099c93b243dcf52e973e19ac7a9b339f50feb2a9
Yar333k <Yaroslav.Uskov1@yandex.ru>
2020-09-21 22:22:17 +0500

    inital commit

---
+++ lab_0.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace lab_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст, который вы хотите отобразить:");
+            string inp = Console.ReadLine();
+            Console.WriteLine(inp);
+            Console.ReadKey();
+        }
+    }
+}
